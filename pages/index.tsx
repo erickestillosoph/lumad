1a7444cecd050907a0b86c20baf852b5b04616e8
unknown <gbrigoli@parlaygames.com>
2023-02-04 17:35:40 +0800

    Initial

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div``;
+
+const HomePage = () => {
+  return <StyledDiv>Welcome to Next.js!</StyledDiv>;
+};
+
+export default HomePage;
